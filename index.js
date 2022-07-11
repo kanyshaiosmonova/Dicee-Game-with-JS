5a6d4cf730a323b25f8e59302d87b4a6fa413c5b
Kanyshai Osmonova <osmonova.kanyshai@gmail.com>
2022-07-11 19:57:26 +0600

    finish the project

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-let randomNumber1 = Math.floor(Math.random()*6) + 1;
+let randomNumber1 = Math.floor(Math.random() * 6)+ 1;
+let randomImage1 = "dice" + randomNumber1 + ".png";
+let randomImageSource1 = "images/" + randomImage1;
+let image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource1);
+
+
+
+let randomNumber2 = Math.floor(Math.random()*6)+1;
+let randomImage2 = "dice" + randomNumber2 + ".png";
+let randomImageSource2 = "images/dice" + randomNumber2+ ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if( randomImage1 > randomImage2) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+} else if (randomImage2 > randomImage1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
